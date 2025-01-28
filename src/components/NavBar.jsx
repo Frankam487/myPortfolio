2cc52d052cc7fec06b950f1ed8ba0e7ad3abc1a6
Frankam487 <fkamgang06@gmail.com>
2025-01-28 16:46:24 +0100

    background added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,25 @@
 import logo from '../assets/inauguration_gaming_campus_photo.jpg';
-import {FaLinkedin, FaTwitter, FaFacebook, FaInstagram} from 'react-icons/fa';
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+} from "react-icons/fa";
 const NavBar = () => {
   return (
-
-      <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-          <img src={logo} alt="logo" />
-        </div>
-      </nav>
-
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="">
+        <FaFacebook className="text-2xl mx-2" />
+        <FaGithub />
+        <FaInstagram />
+        <FaTwitter />
+        <FaLinkedin />
+      </div>
+    </nav>
   );
 };
 
