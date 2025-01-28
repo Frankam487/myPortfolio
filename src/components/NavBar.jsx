8ae631c31d4e952a2d4ff5d19947cf379da96c0d
Frankam487 <fkamgang06@gmail.com>
2025-01-28 15:22:17 +0100

    icons added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
-import logo from '../assets/inauguration_gaming_campus_photo.jpg'
+import logo from '../assets/inauguration_gaming_campus_photo.jpg';
+import {FaLinkedin, FaTwitter, FaFacebook, FaInstagram} from 'react-icons/fa';
 const NavBar = () => {
   return (
-   
+
       <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
           <img src={logo} alt="logo" />
