beb0ab0c910fca6abb173183bc6916977fe3a23a
Frankam487 <fkamgang06@gmail.com>
2025-01-30 14:06:29 +0100

    three js added

--- src/components/TypingText.jsx
+++ src/components/TypingText.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+
 import Typewriter from "typewriter-effect";
 
 const TypingText = () => {
   return (
     <div className="flex justify-center items-center">
-      <Typewriter
+      <Typewriter 
         options={{
           strings: [
             "Hey Yo!!! je suis <span className='bg-white text-9xl'> Frank_Kamgang</span>. Je conçois des Applications web modernes et réactives avec React, TailwindCSS et JavaScript. Quand je ne code pas, je suis souvent à la recherche de nouvelles technologies ou en train de travailler sur des projets créatifs. Ensemble, faisons de votre vision une réalité !",
